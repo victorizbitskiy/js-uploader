946e140d6d87d726ee83b484c95a0ea3047091ea
victorizbitskiy <victor.izbitskiy@yandex.ru>
2021-12-19 09:46:43 +0300

    added input

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-import './upload.js'
+import { upload } from './upload.js'
 
-console.log('app.js');
+upload('#file', {
+  multi: true,
+  accept: ['.png', '.jpg', '.jpeg', '.gif']
+})
--- upload.js
+++ upload.js
@@ -1,1 +1,28 @@
-console.log('upload.js');
+export function upload(selector, options = {}) {
+  const input = document.querySelector(selector)
+
+  const open = document.createElement('button')
+  open.classList.add('btn')
+  open.textContent = 'Открыть'
+
+  if (options.multi) {
+    input.setAttribute('multiple', true)
+  }
+
+  if (options.accept && Array.isArray(options.accept)) {
+    input.setAttribute('accept', options.accept.join(','))
+  }
+
+  input.insertAdjacentElement('afterend', open)
+
+
+
+  const triggerInut = () => input.click()
+
+  const changeHandler = event => {
+
+  }
+
+  open.addEventListener('click', triggerInut)
+  input.addEventListener('change', changeHandler)
+}
